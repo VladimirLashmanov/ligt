503053ea7a90d9d2f9f7b021e5cdd6913e5749ea
VladimirLashmanov <vov7las@gmail.com>
2022-12-26 17:28:44 +0200

    wer

---
+++ traffic-light.js
@@ -0,0 +1,32 @@
+const LIGHTS = {
+    red: 'red',
+    yellow: 'yellow',
+    green:'green'
+};
+const CLASSES_BY_LIGHT ={
+    [LIGHTS.red]:'traffic-light__red',
+    [LIGHTS.yellow]:'traffic-light__yellow',
+    [LIGHTS.green]:'traffic-light__green'
+};
+
+const NEXT_LIGHT_BY_LIGHT={
+    [LIGHTS.red]: LIGHTS.green,
+    [LIGHTS.green]: LIGHTS.yellow,
+    [LIGHTS.yellow]: LIGHTS.red,
+};
+
+let currentLight = LIGHTS.red;
+
+
+function  switchLight(node){
+    currentLight = NEXT_LIGHT_BY_LIGHT[currentLight];
+const currentClass =  CLASSES_BY_LIGHT[currentLight];
+const nextLight =NEXT_LIGHT_BY_LIGHT[currentLight]
+const nextClass =  CLASSES_BY_LIGHT[nextLight];
+
+  node.classList.replace(currentClass, nextClass);
+}
+
+ function  initLight (node){
+  node.classList.add(CLASSES_BY_LIGHT[currentLight]);
+ }
